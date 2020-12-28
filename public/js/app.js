3086262fd4b1835c06b4889ef89a2ab37a469bea
Scott Solmonson <ssolmonson63@gmail.com>
2020-12-28 18:43:51 -0500

    Create main app script
    This will be used to initialize elements that will be manipulated and
    use validation rules so the fields in the form will not be left blank
    by the user.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,28 @@
 window.addEventListener('load', () => {
   // Put all client-side code here
+
+  // Chat platform
+  const chatTemplate = Handlebars.compile($('#chat-template').html())
+  const chatContentTemplate = Handlebars.compile($('#chat-content-template').html())
+  const chatEL = $('#chat')
+  const formEl = $('.form')
+  const messages =[]
+  let username
+
+  // Local Video
+  const localImageEl = $('#local-image')
+  cosnt localVideoEl = $('#local-video')
+
+  //Remote Videos
+  const remoteVideoTemplate = Handlebars.compile($('#remote-video-template').html())
+  const remoteVideosEl = $('#remote-videos')
+  let remoteVideosCount = 0
+
+  // Add validation rules to Create/Join Room Form
+  formEl.form({
+    fields: {
+      roomName: 'empty',
+      username: 'empty'
+    },
+  })
 })
